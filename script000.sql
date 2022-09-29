a56fb7634580404fab6b9ed0f33bd2bbae15039b
Mahonri Maia <mahonri.maia@gmail.com>
2022-09-29 09:02:17 -0300

    Seção 2: Introdução aos tipos VAR, VARCHAR, ENUM, INT e FLOAT

--- script000.sql
+++ script000.sql
@@ -63,3 +63,30 @@ OLAP - BI
 
 /* VERIFICANDO AS TABELAS DO BANCO */
 SHOW TABLES;
+
+/* DESCOBRINDO COMO É A ESTRUTURA DE UMA TABELA */
+DESC cliente;
+
+/* TIPAGEM */
+/* A tipagem em banco de dados é muito importante pois pode ajudar as consultas ao banco de dados a serem mais performáticas, ou seja, a trazerem os resultados da consulta mais rápido.
+
+Para cada tipo de dado existe um tipo que o define. E o uso do tipo certo para o dado certo pode fazer toda a diferença.
+
+Para palavras existe os tipos CHAR, VARCHAR e TEXT; para arquivos e imagens, o tipo BLOB, para números os tipos FLOAT e INT.
+
+VARCHAR é um tipo de dado variável. O CHAR não varia. Ele sempre ocupa o mesmo espaço especificado na criação da tabela. O VARCHAR pode ocupar menos espaço e esse trabalho para se adequar ao tamanho da palavra que ele armazena pode diminuir a performance do banco. Então em casos onde se sabe que a palavra não vai variar no número de caracteres, é ideal que se use o tipo CHAR.
+
+Por exemplo: colunas de gênero (M ou F), colunas de verdadeiro ou falso (1 ou 0) ou ainda colunas de UF (Unidade Federativa), onde os valores são sempre duas letras que representam as siglas de cada Estado do Brasil (CE, BA, SP, RJ, etc). */
+
+/* TIPO ENUM (Enumeração) */
+/* O tipo ENUM só existe no MySQL. Em outros bancos temos que criar Constraints que aprenderemos mais a frente! */
+/* Utilizamos o tipo ENUM quando queremos limitar os dados em uma coluna. Mais a frente veremos comandos de agregação como o comando COUNT e o comando GROUP BY, que contam e agrupam registros, respectivamente.
+
+Por exemplo: o gênero pode ser armazenado de várias formas, M, Masc, Masculino, F, Fem, Feminino. Todas essas possibilidades podem acabam gerando confusão na hora de armazenar esses valores. Por isso existe o ENUM. Ele vai listar as possíveis opções de modo a controlar quais valores são permitidos naquela coluna. Por exemplo, a coluna pode ser limitada a aceitar apenas M ou F ao invés de aceitar Masculino e Feminino. Ele funciona como um ComboBox, onde aparecem as opções possíveis e o usuário não tem que digitar nada. Ou seja, os dados que entrarão naquela coluna não estarão fora do conjunto definido.
+ */
+
+/* TIPOs NUMÉRICOS: INT e FLOAT
+O FLOAT é o tipo numérico usado para representar números reais (números com casas decimais). Serve para representar números com precisão. Quanto mais casas decimais mais precisão. A quantidade de casas é passada por parâmetro. Exemplo: FLOAT(10,2) quer dizer que haverá números com até 10 casas onde 2 ficarão à direita da vírgula. FLOAT(7,3) quer dizer que haverá números com até 7 casas onde 3 casas ficarão à direita da vírgula.
+
+O INT é usado para representar números inteiros. E ele possui um limite de no máximo 11 caracteres. Para números que não são usados para cálculos, o ideal é não usar o INT e sim outro tipo como o VARCHAR. Exemplo: usar VARCHAR para armazenar CPFs e CNPJs.
+ */
